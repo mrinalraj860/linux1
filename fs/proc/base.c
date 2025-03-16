ac068ea3d2b4e1dda4a7c12f70c04a49b2c67a34
Mrinal Raj <mrinalraj@Mrinals-MacBook-Pro-401.local>
2025-03-16 00:53:29 +0000

    updated base.h

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -3338,7 +3338,12 @@ static const struct file_operations proc_task_operations;
 static const struct inode_operations proc_task_inode_operations;
 
 static const struct pid_entry tgid_base_stuff[] = {
-	NOD("fault_stats", S_IRUGO, proc_pid_fault_stats),
+	{
+		.name = "fault_stats",
+		.len = sizeof("fault_stats") - 1,
+		.mode = S_IRUGO,
+		.op.proc = proc_pid_fault_stats,
+	},
 	DIR("task", S_IRUGO | S_IXUGO, proc_task_inode_operations,
 	    proc_task_operations),
 	DIR("fd", S_IRUSR | S_IXUSR, proc_fd_inode_operations,
